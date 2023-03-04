106d4b333ef2749e0af7f4b4c36324cd30410cc9
bks00jk <kseniia.sukhovii@ukma.edu.ua>
2023-03-04 21:01:29 +0200

    Add project files.

---
+++ Lab1BirthdayApp/AnalizeDate.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace KMA.ProgrammingInCSharp2023
+{
+    public class AnalizeDate
+    {
+        #region Fields
+        private DateTime _birthDate;
+        private int _age;
+        #endregion
+
+        public AnalizeDate(DateTime birthDate)
+        {
+            this._birthDate = birthDate;
+            this._age = GetAge();
+
+        }
+    
+        public int GetAge()
+        {
+            var currentDate = DateTime.Today;
+
+            int age = currentDate.Year - _birthDate.Year;
+
+            if (_birthDate > currentDate.AddYears(-age))
+                age--;
+
+            return age;
+        }
+
+        public bool IsValidAge()
+        {
+            return (_age >= 0 && _age <= 135);
+        }
+
+       
+        public bool IsBirthday()
+        {
+            return (_birthDate.Month == DateTime.Today.Month && _birthDate.Day == DateTime.Today.Day);
+           
+        }
+
+        public string GetWesternSign()
+        {
+
+            var day = _birthDate.Day;
+            var month = _birthDate.Month;
+
+            switch (month)
+            {
+                case 1: 
+                    return day <= 20 ? "Capricorn" : "Aquarius";
+                case 2: 
+                    return day <= 19 ? "Aquarius" : "Pisces";
+                case 3: 
+                    return day <= 20 ? "Pisces" : "Aries";
+                case 4: 
+                    return day <= 20 ? "Aries" : "Taurus";
+                case 5: 
+                    return day <= 21 ? "Taurus" : "Gemini";
+                case 6: 
+                    return day <= 21 ? "Gemini" : "Cancer";
+                case 7:
+                    return day <= 22 ? "Cancer" : "Leo";
+                case 8:
+                    return day <= 23 ? "Leo" : "Virgo";
+                case 9:
+                    return day <= 23 ? "Virgo" : "Libra";
+                case 10:
+                    return day <= 23 ? "Libra" : "Scorpio";
+                case 11:
+                    return day <= 22 ? "Scorpio" : "Sagittarius";
+                case 12:
+                    return day <= 21 ? "Sagittarius" : "Capricorn";
+                default:
+                    throw new ArgumentException("Invalid birth month.");
+            }
+        }
+
+        public string GetChineseSign()
+        {
+            string[] zodiacSigns = { "Monkey", "Rooster", "Dog", "Pig", "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Sheep" };
+            int zodiacIndex = _birthDate.Year % 12;
+            return zodiacSigns[zodiacIndex];
+        }
+    }
+}
---
+++ Lab1BirthdayApp/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KMA.ProgrammingInCSharp2023.Lab1BirthdayApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        #region Fields
+        private DateTime _birthDate;
+        private string _age;
+        private string _westernSign;
+        private string _chineseSign;
+        #endregion
+
+        #region Properties
+        public DateTime BirthDate
+        {
+            get => _birthDate;
+            set
+            {
+                _birthDate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Age
+        {
+            get => _age;
+            set
+            {
+                _age = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string WesternSign
+        {
+            get => _westernSign;
+            set
+            {
+                _westernSign = value;
+                OnPropertyChanged(nameof(WesternSign));
+            }
+        }
+
+        public string ChineseSign
+        {
+            get => _chineseSign;
+            set
+            {
+                _chineseSign = value;
+                OnPropertyChanged(nameof(ChineseSign));
+            }
+        }
+        #endregion
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+        #region Button Clicks
+        private void GetAge_Click(object sender, RoutedEventArgs e)
+        {
+            BirthDate = (DateTime)datePicker.SelectedDate;
+            AnalizeDate analizeDate = new AnalizeDate(BirthDate);
+            int age = analizeDate.GetAge();
+            if (!analizeDate.IsValidAge())
+            {
+                MessageBox.Show("Invalid date of birth!");
+            }
+            else if (analizeDate.IsBirthday())
+            {
+                Age = $"You are {age} today! \nHappy Birthday! \nBe happy! \nGlory to Ukraine!";
+            }
+            else
+            {
+                Age = $"You are {age} y.o.";
+            }
+            WesternSign = "";
+            ChineseSign ="";
+        }
+
+        private void GetSign_Click(object sender, RoutedEventArgs e)
+        {
+            BirthDate = (DateTime)datePicker.SelectedDate;
+            AnalizeDate analizeDate = new AnalizeDate(BirthDate);
+            if (!analizeDate.IsValidAge())
+            {
+                MessageBox.Show("Invalid date of birth!");
+            }
+            else
+            {
+                string westernSign = analizeDate.GetWesternSign();
+                string chineseSign = analizeDate.GetChineseSign();
+                WesternSign = $"Your Western Sign: {westernSign}";
+                ChineseSign = $"Your Chinese Sign: {chineseSign}";
+            }
+           
+            Age = "";
+        }
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
